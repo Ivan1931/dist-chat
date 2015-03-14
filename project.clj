5c5b61cd47a7f629c28738b23ca760d33b7505e8
jonah-hooper <jonah.graham.hooper@gmail.com>
2015-03-14 21:31:31 +0200

    Refactored command tests

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :test-main dist-chat.core-test
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/data.json "0.2.6"]
+  :dependencies [[org.clojure/tools.trace "0.7.8"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [org.clojure/clojure "1.6.0"]])
--- test/dist_chat/core_test.clj
+++ test/dist_chat/core_test.clj
@@ -1,5 +1,6 @@
 (ns dist-chat.core-test
-  (:require [clojure.data.json :as json]
+  (:require [clojure.tools.trace :refer :all]
+            [clojure.data.json :as json]
             [clojure.string :as string]
             [clojure.test :refer :all]
             [dist-chat.core :refer :all]))
@@ -11,28 +12,58 @@
 (def expected-msg (str (json/write-str {:message test-message}) "\n" :done))
 (def send-msg-test-string (str (json/write-str {:send-message {:port reciever-port :host "localhost" :message test-message}}) "\n" :done))
 
-(defmacro dbg
-  [debug-message & form]
-  `(do (println ~debug-message)
-      ~form))
 
-(defn fill-msg-dispatch
+;; So what do we need to do?
+;; Need to have a way to capture the output of a command
+;; -> Use a promis. Promis is fulled by some function with the output of our command
+;; Need to start a command server
+;; -> Pretty straight forward
+;; May want to setup some sort of mock state for the command to run 
+;; -> The state will be stored in a bunch of in memory atoms. Perhaps we might want to assertain the value of that approach
+;; -> Defintely cannot be thread local. So these will have to be globally available atoms. This will be the situation for now
+;; -> Need a function to setup the state
+;; Actually, all we need to do is to write to the promise where to input is expected
+;; So we merely have to pass a function that writes to a promise
+;; Perhaps we will also want to include a timeout facility so that we can fail the test instead of blocking the main thread
+
+(defmacro before
+  [message & form]
+  `(do (println "Message is: " ~message)
+       ~form))
+
+(defn run-controller-test
+  [controller-port setup prom timeout command-resolver & args]
+  (do setup
+      (let [controller-server (future (create-controller-server controller-port))
+            command-worker (future (apply command-resolver args))]
+        (do (Thread/sleep timeout)
+            (do-repeatedly future-cancel controller-server command-worker)
+            (if (realized? prom)
+              prom
+              (deliver prom :timeout))))))
+
+(defn message-reciever-dispatch
   [socket]
   (let [lines (string/join "\n" (read-lines socket 2))]
     (do (deliver msg lines)
         (.close socket))))
 
-(defn setup-command-dispatch-test
-  [controller-port reciever-port]
-  (do (future (create-controller-server controller-port))
-      (future (create-server reciever-port fill-msg-dispatch))
-      (Thread/sleep 100)))
+(defn send-and-recieve-message
+  [controller-port reciever-port message-command]
+  (let [reciever-worker (future (create-server reciever-port 
+                                               message-reciever-dispatch))
+        socket (create-socket "localhost" controller-port)
+        write-worker (write-to socket message-command)]
+    (do (write-worker)
+        (Thread/sleep 100)
+        (.close socket)
+        (do-repeatedly future-cancel reciever-worker write-worker))))
 
-(setup-command-dispatch-test controller-port reciever-port)
-
-(let [socket (create-socket "localhost" controller-port)]
-      (write-to socket send-msg-test-string)
-      (.close socket))
+(run-controller-test controller-port nil msg 1000
+                  send-and-recieve-message 
+                  controller-port
+                  reciever-port 
+                  send-msg-test-string)
 
 (deftest dispatch-message-send-test
   (is (= @msg expected-msg)))
